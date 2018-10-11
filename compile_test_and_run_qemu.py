87255cb281c956f5c7c6ae13c2dd2c401b891eef
Oren Milman <orenmn@gmail.com>
2018-10-11 18:58:20 +0300

    added (somewhat ugly) waiting until output file is created, then reading it.

--- compile_test_and_run_qemu.py
+++ compile_test_and_run_qemu.py
@@ -1,11 +1,26 @@
 import subprocess
+import os
+import os.path
+import time
 
 
 TEST_SOURCE_PATH = '/mnt/hgfs/CSL_QEMU_memory_tracer/qemu_automation/test.c'
 RUN_QEMU_SCRIPT_PATH = '/mnt/hgfs/CSL_QEMU_memory_tracer/qemu_automation/run_qemu.bash'
 TEST_ELF_NAME = 'test_elf'
+TEST_OUTPUT_PATH = 'test_output.txt'
 
 
+def read_txt_file_when_it_exists(file_path):
+    while not os.path.isfile(file_path):
+        time.sleep(1)
+    return read_txt_file(file_path)
+
+def read_txt_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+os.remove(TEST_OUTPUT_PATH)
 
 subprocess.run('cd ~', shell=True, check=True)
 
@@ -15,4 +30,9 @@ compile_cmd = (f'gcc -Werror -Wall -pedantic '
 subprocess.run(compile_cmd, shell=True, check=True)
 
 subprocess.run(RUN_QEMU_SCRIPT_PATH, shell=True, check=True)
+
+
+output = read_txt_file_when_it_exists(TEST_OUTPUT_PATH)
+
+
 
