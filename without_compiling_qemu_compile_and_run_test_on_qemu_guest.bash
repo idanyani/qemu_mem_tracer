f5539988a8c7837c0403c68013c074b1733b4f43
Oren Milman <orenmn@gmail.com>
2018-10-16 23:28:59 +0300

    Delete without_compiling_qemu_compile_and_run_test_on_qemu_guest.bash

--- without_compiling_qemu_compile_and_run_test_on_qemu_guest.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3.7 /mnt/hgfs/qemu_automation/compile_and_run_test_on_qemu_guest.py oren_vm_disk2.qcow2 ready_for_test2 /mnt/hgfs/qemu_automation/test.c 123456 ~/qemu_mem_tracer
