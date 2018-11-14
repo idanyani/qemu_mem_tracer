7c099525648660ab6ce52423d466f5f2ef62a60a
Oren Milman <orenmn@gmail.com>
2018-11-14 14:37:59 +0200

    cleanup after making serial work..

--- host_guest_workload_communications/write_script_to_serial.py
+++ host_guest_workload_communications/write_script_to_serial.py
@@ -1,7 +1,5 @@
 import argparse
 import time
-# import struct
-import serial
 import subprocess
 
 SYNC_BYTES = b'serial sync\n'
@@ -37,41 +35,13 @@ if __name__ == '__main__':
     assert(len(script_contents_as_hex) == script_size * 2)
     print(script_contents_as_hex)
 
-    # echo -e "\012"
-    # execute_cmd(f'echo -en "\x41"'
-    #             f' > {args.serial_port_path}')
-    # execute_cmd(f'echo -en "{get_byte_repr_for_serial(dont_add_communications)}"'
-    #             f' > {args.serial_port_path}')
-
-    # execute_cmd(f'echo -en "{get_bytes_repr_for_serial(script_size_bytes)}"'
-    #             f' > {args.serial_port_path}')
-
-    # execute_cmd(f'cat {args.script_path} > {args.serial_port_path}')
-
     with open(args.serial_port_path, 'wb') as f:
         f.write(SYNC_BYTES)
         f.write(dont_add_communications_bytes)
         f.write(script_size_bytes)
-        # print(script_size * 3)
         for i in range(0, len(script_contents_as_hex), 2):
+            # time.sleep(0.00001)
             hex_repr_and_line_feed_bytes = (
                 f'{script_contents_as_hex[i:i+2]}\n'.encode('ascii'))
-            # time.sleep(0.00001)
-            # print(i)
             f.write(hex_repr_and_line_feed_bytes)
-
-    # print('aoeu')
-
-
-    # ser = serial.Serial(args.serial_port_path, baudrate=115200)
-    # if not ser.isOpen():
-    #     raise RuntimeError(f'Failed to open serial {args.serial_port_path}')
-    
-    # ser.write(SYNC_BYTES)
-    # print(SYNC_BYTES)
-    # ser.write(dont_add_communications_bytes)
-    # print(dont_add_communications_bytes)
-    # ser.write(script_size_bytes)
-    # print(script_size_bytes)
-    # ser.write(script_contents.hex().encode('ascii'))
 
